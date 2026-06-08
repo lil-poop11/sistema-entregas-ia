031a41e1b0d82a2c6fc6e80b1edcea32a5fa91c3
Rafael Callou <144628725+lil-poop11@users.noreply.github.com>
2026-06-08 16:38:03 -0300

    Add routing logic in index.php for controller actions

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+require_once "../app/controllers/EntregaController.php";
+
+$controller = new EntregaController();
+
+$rota = $_GET['rota'] ?? 'home';
+
+switch ($rota) {
+
+    case 'entregas':
+        $controller->index();
+        break;
+
+    case 'salvar':
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $controller->store();
+        }
+        break;
+
+    case 'home':
+    default:
+        include "../app/views/home.php";
+        break;
+}
