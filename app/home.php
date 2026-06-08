0f37a696c421b6606b5feb72319919e9d2487a4d
Rafael Callou <144628725+lil-poop11@users.noreply.github.com>
2026-06-08 16:27:16 -0300

    Add home.php with basic HTML structure

---
+++ app/home.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <h1>Sistema de Entregas</h1>
+    <a href="index.php?rota=entregas">Ver Entregas</a>
+</body>
+</html>
